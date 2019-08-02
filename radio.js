330f22fa51784fa26d9f2fa9d3ada7d3793716db
JD <jd@lafaceb.live>
2019-08-01 23:10:51 -0400

    Stripped down minimal player

--- radio.js
+++ radio.js
@@ -1,32 +1,25 @@
 $(
-  function(){
+function() {
 
-var previous = 'first load';
-var current = null;
+    var previous = 'first load';
+    var current = null;
 
-songinfo();
-
-setInterval(function() {
-	songinfo();
-}, 3000);
-
-function songinfo(){
-}
-
-var aud = $('audio')[0];
-$('#ppbutton').on('click', function(){
-	if (aud.paused) {
-		aud.play();
-		$('#ppbutton').removeClass('fas fa-play');
-		$('#ppbutton').addClass('fas fa-pause');
-		location.hash = '#play';
-	}
-	else {
-		aud.pause();
-		$('#ppbutton').removeClass('fas fa-pause');
-		$('#ppbutton').addClass('fas fa-play');
-		location.hash = '#stop';
-	}
-});
+    var aud = $('audio')[0];
+    $('#ppbutton').on('click', function() {
+        // if (aud.paused) {
+        if (aud.muted) {
+            aud.muted = false;
+            $('#ppbutton').removeClass('fas fa-play');
+            $('#ppbutton').addClass('fas fa-pause');
+            location.hash = '#play';
+        }
+        else {
+            // aud.pause();
+            aud.muted = true;
+            $('#ppbutton').removeClass('fas fa-pause');
+            $('#ppbutton').addClass('fas fa-play');
+            location.hash = '#pause';
+        }
+    });
 
 });
