baf698d7af5b3893f68d819f046515906c11ca1e
JD <jd@lafaceb.live>
2019-08-01 22:48:28 -0400

    Initial Commit

--- radio.js
+++ radio.js
@@ -11,35 +11,20 @@ setInterval(function() {
 }, 3000);
 
 function songinfo(){
-
-		if(location.hash == '#play' && $('audio')[0].paused){
-			$('audio')[0].play();
-			$('#ppbutton').removeClass('icon-play-circled2');
-			$('#ppbutton').addClass('icon-pause-circle-o');
-		}
-		$.getJSON("/song.json", {t: new Date().getTime()}, function(json) {
-				current = JSON.stringify(json);            
-				if (previous && current && previous !== current) {
-					$( "ul#info" ).replaceWith('<ul id="info"><li class="icon-music">'+json.song+'</li><li class="icon-book">'+json.album+'</li><li class="icon-user">'+json.artist+'</li></ul>');
-					$( "img#cover" ).replaceWith('<img id="cover" src="'+json.cover+'">');
-					$(document).attr("title", 'radio - '+json.artist+' '+json.album+' '+json.song);
-				}
-				previous = current;
-		});                       
 }
 
 var aud = $('audio')[0];
 $('#ppbutton').on('click', function(){
 	if (aud.paused) {
 		aud.play();
-		$('#ppbutton').removeClass('icon-play-circled2');
-		$('#ppbutton').addClass('icon-pause-circle-o');
+		$('#ppbutton').removeClass('fas fa-play');
+		$('#ppbutton').addClass('fas fa-pause');
 		location.hash = '#play';
 	}
 	else {
 		aud.pause();
-		$('#ppbutton').removeClass('icon-pause-circle-o');
-		$('#ppbutton').addClass('icon-play-circled2');
+		$('#ppbutton').removeClass('fas fa-pause');
+		$('#ppbutton').addClass('fas fa-play');
 		location.hash = '#stop';
 	}
 });
